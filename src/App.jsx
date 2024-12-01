f116ca8e5bedb21e65d4fd46f63a99bfbf997a34
DKNY88 <dhirennangla@gmail.com>
2024-12-01 21:39:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/header.jsx';
+import CoreConcepts from './components/coreconcepts.jsx';
+import Examples from './components/examples.jsx';
+
+
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <CoreConcepts></CoreConcepts>
+        <Examples></Examples>
+      </main>
+    </>
+  );
+}
+
+
+export default App;
